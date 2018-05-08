0801e0d9e7bab733c5d90c832373676dd00b9c64
eddieharmonjr <eddieharmon@ac.c4q.nyc>
2018-05-08 16:20:49 -0400

    create preliminary login form.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -9,6 +9,21 @@ class Login extends Component {
         return(
             <div>
                 <h1>Welcome to the Tenantable Login Page!</h1>
+
+                <div>
+                    <form action="/action_page.php">
+                        <fieldset>
+                            <legend>Welcome Back! Login Here:</legend>
+                                Username:<br/>
+                            <input type="text" name="username" value="Username"/><br/>
+                            <br/>
+                                Password:<br/>
+                            <input type="password" name="password" value="Password"/><br/><br/>
+                            <input type="submit" value="Login" />
+                        </fieldset>
+                    </form>
+                </div>
+
             </div>
         )
     }
